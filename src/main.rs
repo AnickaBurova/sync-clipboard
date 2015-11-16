1e39e0f577e1194d3130b1eb0a8a5f0c9e616aa8
Milan Buransky <milan.buransky@ims-evolve.com>
2015-11-16 15:12:47 +0000

    Synchronising clipboards.

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,14 @@ static HOST:&'static str = "127.0.0.1:24011";
 
 fn run_reading(stream : &mut TcpStream,content : Sender<String>) -> Result<()>{
     loop {
+        println!("Waiting for a data");
         let length : u32 = try!(stream.read_u32::<LittleEndian>());
+        println!("Got the data size: {}", length);
         let mut data = vec![0u8;length as usize];
         try!(stream.take(length as u64).read_to_end(&mut data));
-
         match String::from_utf8(data){
             Ok(s) => {
+                println!("Received new content from the other side: {}", s);
                 let _ = content.send(s);
             }
             Err(_) => {
@@ -33,7 +35,7 @@ fn run_reading(stream : &mut TcpStream,content : Sender<String>) -> Result<()>{
     }
 }
 
-fn run_sync(stream : TcpStream) -> Result<()>{
+fn run_sync(stream : &mut TcpStream) -> Result<()>{
     let mut ctx = match ClipboardContext::new(){
         Ok(c) => c,
         Err(_) => return Err(Error::new(ErrorKind::Other,"Failed to create clipboard context"))
@@ -48,15 +50,37 @@ fn run_sync(stream : TcpStream) -> Result<()>{
     let (wx, rx) = channel::<String>();
     // // create thread which will read the stream for changes from outside, and apply changes to this system
     //
-    thread::spawn(move||{run_reading(&mut stream.try_clone().unwrap(),wx)});
+    let mut reading = stream.try_clone().unwrap();
+    thread::spawn(move||{run_reading(&mut reading,wx)});
+    let mut writer = stream.try_clone().unwrap();
+
+    println!("initial clipboard value is: {}",current_content );
 
     loop{
         thread::sleep(Duration::new(1,0));
+        println!("Checking for new content!");
         match ctx.get_contents(){
             Ok(s) => {
+
                 if s != current_content{
+                    println!("A new content, sending it to the other side: {}!={}",s,current_content);
+                    current_content = s.clone();
                     let data = s.as_bytes();
-                    // stream.write(data);
+                    try!(writer.write_u32::<LittleEndian>(data.len() as u32));
+                    try!(writer.write(data));
+                }
+                match rx.try_recv(){
+                    Ok(v) =>{
+                        println!("Received a new value for cb: {}", v );
+                        if v != current_content{
+                            current_content = v.clone();
+                            match ctx.set_contents(v){
+                                Ok(_) => println!("Seting the new value as clipboard content."),
+                                Err(_) => println!("Failde to set the new value as clipboard content.")
+                            }
+                        }
+                    }
+                    Err(_) => ()
                 }
             }
             Err(_) => ()
@@ -67,7 +91,7 @@ fn run_sync(stream : TcpStream) -> Result<()>{
 
 fn try_run_client() -> Result<()>{
     let mut stream = try!(TcpStream::connect(&HOST));
-    run_sync(stream)
+    run_sync(&mut stream)
 }
 
 
@@ -79,7 +103,7 @@ fn run_server() -> Result<()> {
             Ok(stream) => {
                 thread::spawn(move||{
                     println!("connected");
-                    run_sync(stream)
+                    run_sync(&mut stream.try_clone().unwrap())
                 });
             }
             Err(e) => {
